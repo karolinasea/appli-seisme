e0a5a55d249ab485243881486b8f12c39d11b356
karolinasea <46023630+karolinasea@users.noreply.github.com>
2019-06-11 09:39:26 +0700

    added longitude and latitude (coordinates) attributes and their set and get methods

--- Seisme.java
+++ Seisme.java
@@ -7,6 +7,8 @@ public class Seisme {
     private String place;
     private Date time;
     private double magnitude;
+    private double coordinate1;
+	private double coordinate2;
 
 
     public Seisme(String t, String p, int d, double m){
@@ -41,5 +43,12 @@ public class Seisme {
     public void setMagnitude(double s){
         magnitude = s;
     }
+    
+    public Double getCoordinate1() { return coordinate1; }
+	public Double getCoordinate2() { return coordinate2; }
 
+    public void setCoordinate1(Double c){ coordinate1 = c; }
+	public void setCoordinate2(Double c){ coordinate2 = c; }
+    
 }
+
